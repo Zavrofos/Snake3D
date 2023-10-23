2d600299edd9722b8333d716da2248e60d1611fa
Rebrin <rebrin.na@gmail.com>
2023-10-23 14:05:46 +0600

    Added Movement Snake

---
+++ Assets/Prototipe/GravitationalAttractor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Prototipe
+{
+    public class GravitationalAttractor : MonoBehaviour
+    {
+        public float GravitationalForce = 10.0f; 
+        public GameObject Apple;
+
+        void FixedUpdate()
+        {
+            if (Apple != null)
+            {
+                Vector3 directionToAttractor = Apple.transform.position - transform.position;
+                float gravity = GravitationalForce / directionToAttractor.sqrMagnitude;
+                GetComponent<Rigidbody>().AddForce(directionToAttractor.normalized * gravity);
+            }
+        }
+    }
+}
