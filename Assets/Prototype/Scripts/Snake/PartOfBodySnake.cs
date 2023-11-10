c867bbf187c2c6ea650d01f73a9b5b5820c30d12
Rebrin <rebrin.na@gmail.com>
2023-11-10 20:33:56 +0600

    Created PartOfBody Prefab

---
+++ Assets/Prototype/Scripts/Snake/PartOfBodySnake.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prototype.Scripts.Snake
+{
+    public class PartOfBodySnake : MonoBehaviour
+    {
+        [HideInInspector] public Vector3 PreviousPosition;
+    }
+}
