6e506f07914c5c400ddfd31a5b32b76fc0e3e78e
Rebrin <rebrin.na@gmail.com>
2023-11-10 20:34:16 +0600

    Created BodySnake

---
+++ Assets/Prototype/Scripts/Snake/BodySnake.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Prototype.Scripts.Snake
+{
+    public class BodySnake : MonoBehaviour
+    {
+        public List<PartOfBodySnake> Body;
+        public PartOfBodySnake PartOfBodyPrefab;
+
+        public void UpdateGame(Vector3 previousSnakeHeadPosition)
+        {
+            for (var i = 0; i < Body.Count; i++)
+            {
+                var part = Body[i];
+                var partTransform = part.transform;
+                part.PreviousPosition = partTransform.position;
+                if (i == 0)
+                {
+                    partTransform.position = previousSnakeHeadPosition;
+                }
+                else
+                {
+                    partTransform.position = Body[i - 1].PreviousPosition;
+                }
+            }
+        }
+    }
+}
