00f53b404253cad806fe21c0a08ad273335efdeb
Rebrin <rebrin.na@gmail.com>
2023-11-12 00:53:01 +0600

    Changed RotationHead

---
+++ Assets/Prototype/Scripts/Snake/HeadSnake.cs
@@ -0,0 +1,40 @@
+using Prototype.Scripts.SpawnerFoodFolder;
+using Prototype.Scripts.Touch;
+using UnityEngine;
+
+namespace Prototype.Scripts.Snake
+{
+    public class HeadSnake : MonoBehaviour
+    {
+        private readonly float _radiusFindFood = 3;
+
+        public LerpDirection LerpDirection;
+        [SerializeField] private LayerMask _foodLayerMask;
+        [SerializeField] private BodySnake _bodySnake;
+        [SerializeField] private SpawnerFood _spawnerFood;
+        
+        private void Update()
+        {
+            float angle = LerpDirection.Angle * Mathf.Rad2Deg;
+            transform.localRotation = Quaternion.Euler(new Vector3(0, angle,0));
+            FindFood();
+        }
+
+        public void FindFood()
+        {
+            var food = Physics.OverlapSphere(transform.position, _radiusFindFood, _foodLayerMask);
+            foreach (var part in food)
+            {
+                var partOfFood = part.GetComponent<Food>();
+                var direction = (transform.position - part.transform.position).normalized;
+                partOfFood.Move(direction);
+                if (Vector3.Distance(transform.position, part.transform.position) < 0.5)
+                {
+                    Destroy(part.gameObject);
+                    _bodySnake.SpawnPartOfBodySnake();
+                    _spawnerFood.SpawnFood();
+                }
+            }
+        }
+    }
+}
