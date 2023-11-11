b85ed1e3824fed88887cddd2928634fbe423daac
Rebrin <rebrin.na@gmail.com>
2023-11-12 00:06:37 +0600

    Refactoring SpawnerFood

--- Assets/Prototype/Scripts/SpawnerFood/SpawnerFood.cs
+++ Assets/Prototype/Scripts/SpawnerFood/SpawnerFood.cs
@@ -7,46 +7,54 @@ namespace Prototype.Scripts.SpawnerFood
 {
     public class SpawnerFood : MonoBehaviour
     {
-        [SerializeField] private int _initialCountFood;
-        [SerializeField] private LayerMask _appleLayerMask;
-        [SerializeField] private Food _foodPrefab;
-        private List<Food> _foodActive;
+        private readonly int _initialCountFood = 30;
+        private readonly float _radiusCheckNearFood = 5;
+        private readonly int _lengthRay = 50;
 
+        [SerializeField] private LayerMask _appleLayerMask;
+        [SerializeField] private LayerMask _foodLayerMask;
+        [SerializeField] private Food _foodPrefab;
+        
         private void Start()
         {
-            _foodActive = new List<Food>();
-
             for (int i = 0; i < _initialCountFood; i++)
             {
                 SpawnFood();
             }
         }
 
-        private void SpawnFood()
+        public void SpawnFood()
+        {
+            Vector3 position;
+            do
+            {
+                position = GetRandomPosition();
+            } while (IsThereFoodNearby(position));
+            Food food = Instantiate(_foodPrefab, position, Quaternion.identity);
+        }
+
+        private bool IsThereFoodNearby(Vector3 position)
+        {
+            var foods = Physics.OverlapSphere(position, _radiusCheckNearFood, _foodLayerMask);
+            return foods.Length != 0;
+        }
+
+        private Vector3 GetRandomPosition()
         {
             float x = Random.Range(-1f, 1f);
             float y = Random.Range(-1f, 1f);
             float z = Random.Range(-1f, 1f);
-
+            
             if (x == 0 && y == 0 && z == 0) x = 1;
             
             var direction = new Vector3(x, y, z).normalized;
-            Debug.DrawRay(transform.position, direction * 30, Color.red);
-            if (Physics.Raycast(transform.position + direction * 50, -direction * 50, out var hit,50, LayerMask.GetMask("Apple")))
+            
+            if (Physics.Raycast(transform.position + direction * _lengthRay, -direction * _lengthRay, out var hit,_lengthRay, _appleLayerMask))
             {
-                var position = hit.point + direction;
-                Food food = Instantiate(_foodPrefab, position, Quaternion.identity);
-                food.transform.rotation = Quaternion.FromToRotation(food.transform.up, hit.normal) * food.transform.rotation;
-                _foodActive.Add(food);
+                return hit.point + direction;
             }
-        }
-
-        private void Update()
-        {
-            if (Input.GetKey(KeyCode.Space))
-            {
-                SpawnFood();
-            }
+            
+            return Vector3.zero;
         }
     }
 }
