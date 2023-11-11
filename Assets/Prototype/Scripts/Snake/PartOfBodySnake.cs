bd089464e1711363a9a3047ae055cde968ad9a8a
Rebrin <rebrin.na@gmail.com>
2023-11-11 21:01:33 +0600

    Refactoring PartOfBodySnake

--- Assets/Prototype/Scripts/Snake/PartOfBodySnake.cs
+++ Assets/Prototype/Scripts/Snake/PartOfBodySnake.cs
@@ -7,6 +7,22 @@ namespace Prototype.Scripts.Snake
 {
     public class PartOfBodySnake : MonoBehaviour
     {
-        [HideInInspector] public Vector3 PreviousPosition;
+        public List<PositionAndRotationHolder> PositionsAndRotations;
+        public List<PositionAndRotationHolder> OldPositionsAndRotations;
+        private int _currentPositionIndex;
+        
+        public void Move()
+        {
+            if (PositionsAndRotations == null || PositionsAndRotations.Count == 0) return;
+
+            var transform1 = transform;
+            transform1.position = PositionsAndRotations[_currentPositionIndex].Position;
+            transform1.rotation = PositionsAndRotations[_currentPositionIndex].Rotation;
+            _currentPositionIndex++;
+
+            if (_currentPositionIndex != PositionsAndRotations.Count) return;
+            OldPositionsAndRotations = new List<PositionAndRotationHolder>(PositionsAndRotations);
+            _currentPositionIndex = 0;
+        }
     }
 }
