4684a29124fc8e6f2d857bfdf0929fdc6ad63d39
Rebrin <rebrin.na@gmail.com>
2023-11-13 01:00:32 +0600

    Added CreatePartOfBodyPresenter

---
+++ Assets/Scripts/Snake/Body/CreatePartOfBodyPresenter.cs
@@ -0,0 +1,36 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+
+namespace Snake.Body
+{
+    public class CreatePartOfBodyPresenter : IPresenter
+    {
+        private GameModel _gameModel;
+        private GameView _gameView;
+
+        public CreatePartOfBodyPresenter(GameModel gameModel, GameView gameView)
+        {
+            _gameModel = gameModel;
+            _gameView = gameView;
+        }
+        
+        public void Subscribe()
+        {
+            _gameModel.SnakeModel.Body.CreatedPartOfBody += OnCreatePartOfBodySnake;
+        }
+
+        public void Unsubscribe()
+        {
+            _gameModel.SnakeModel.Body.CreatedPartOfBody -= OnCreatePartOfBodySnake;
+        }
+
+        private void OnCreatePartOfBodySnake()
+        {
+            PartOfBodySnakeView partOfBodySnakeView =
+                GameObject.Instantiate(_gameView.BodySnakeView.PartOfBodySnakePrefab,
+                    _gameView.BodySnakeView.transform);
+            
+            _gameView.BodySnakeView.PartsOfBodySnake.Add(partOfBodySnakeView);
+        }
+    }
+}
