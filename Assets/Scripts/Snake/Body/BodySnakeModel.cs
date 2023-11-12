884aa7b0e2fc013d12a12c2b5a62e76a957a523b
Rebrin <rebrin.na@gmail.com>
2023-11-13 01:01:54 +0600

    Added event and moved to a new folder

---
+++ Assets/Scripts/Snake/Body/BodySnakeModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Snake.Body
+{
+    public class BodySnakeModel
+    {
+        public readonly int GapBetweenPositionsOfBodyParts;
+        public event Action CreatedPartOfBody;
+
+        public BodySnakeModel(int gapBetweenPositionsOfBodyParts)
+        {
+            GapBetweenPositionsOfBodyParts = gapBetweenPositionsOfBodyParts;
+        }
+
+        public void CreatePartOfBody()
+        {
+            CreatedPartOfBody?.Invoke();
+        }
+    }
+}
