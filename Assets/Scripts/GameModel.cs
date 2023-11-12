3d7fdc68ec89b3b83cd4420fec3a80c03d779fb4
Rebrin <rebrin.na@gmail.com>
2023-11-12 18:12:21 +0600

    Init GameModel

--- Assets/Scripts/GameModel.cs
+++ Assets/Scripts/GameModel.cs
@@ -1,8 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Snake;
 using UnityEngine;
 
-public class GameModel 
+public class GameModel
 {
-    
+    public readonly SnakeModel SnakeModel;
+    public GameModel(int initialCountPartOfBody)
+    {
+        SnakeModel = new SnakeModel(initialCountPartOfBody);
+    }
 }
