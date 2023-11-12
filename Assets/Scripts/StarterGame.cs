0dd569f5edbaf1b0996cc90e06fcfc17bce1b590
Rebrin <rebrin.na@gmail.com>
2023-11-12 18:12:31 +0600

    Init StarterGame

--- Assets/Scripts/StarterGame.cs
+++ Assets/Scripts/StarterGame.cs
@@ -1,10 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts;
 using UnityEngine;
 
-namespace Assets.Scripts
+public class StarterGame : MonoBehaviour
 {
-    public class StarterGame : MonoBehaviour
+    [HideInInspector] public GameModel GameModel;
+    public GameView GameView;
+    public int InitialCountoartOfBodySnake;
+
+    private List<IPresenter> _presenters;
+    private List<IUpdater> _updaters;
+
+    private void Start()
     {
+        GameModel = new GameModel(InitialCountoartOfBodySnake);
         
+        _presenters = new List<IPresenter>()
+        {
+
+        };
+        
+        _updaters = new List<IUpdater>()
+        {
+
+        };
+    }
+
+    private void Update()
+    {
+        foreach (var updater in _updaters)
+        {
+            updater.Update();
+        }
+    }
+
+    private void OnEnable()
+    {
+        foreach (var presenter in _presenters)
+        {
+            presenter.Subscribe();
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (var presenter in _presenters)
+        {
+            presenter.Unsubscribe();
+        }
     }
 }
