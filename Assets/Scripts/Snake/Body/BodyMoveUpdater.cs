5d3fbac83756ca9c64ae8ca1d90c3a506d95b389
Rebrin <rebrin.na@gmail.com>
2023-11-13 01:01:00 +0600

    Added BodyMoveUpdater

---
+++ Assets/Scripts/Snake/Body/BodyMoveUpdater.cs
@@ -0,0 +1,42 @@
+﻿using Assets.Scripts;
+using Snake.Head;
+
+namespace Snake.Body
+{
+    public class BodyMoveUpdater : IUpdater
+    {
+        private GameModel _gameModel;
+        private GameView _gameView;
+
+        public BodyMoveUpdater(GameModel gameModel, GameView gameView)
+        {
+            _gameModel = gameModel;
+            _gameView = gameView;
+        }
+        
+        public void Update()
+        {
+            PositionAndRotationHolder holder = new PositionAndRotationHolder(_gameModel.SnakeModel.Head.Position,
+                _gameModel.SnakeModel.Head.Rotation, _gameModel.SnakeModel.Head.IsEatedFood);
+            PositionAndRotationHolder nextHolder = holder;
+
+            var body = _gameView.BodySnakeView.PartsOfBodySnake;
+            
+            for (int i = 0; i < body.Count; i++)
+            {
+                body[i].History.Enqueue(nextHolder);
+                nextHolder = body[i].Move(_gameModel.SnakeModel.Body.GapBetweenPositionsOfBodyParts);
+                if (nextHolder == null) return;
+
+                if (i == body.Count - 1)
+                {
+                    if (nextHolder.IsEatedFood)
+                    {
+                        _gameModel.SnakeModel.Body.CreatePartOfBody();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
