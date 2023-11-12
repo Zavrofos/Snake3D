da3125fb558e54a451ceedd7e9071ab5e317d03b
Rebrin <rebrin.na@gmail.com>
2023-11-12 18:37:34 +0600

    Added List PartsOfBody to BodySnakeModel

--- Assets/Scripts/Snake/BodySnakeModel.cs
+++ Assets/Scripts/Snake/BodySnakeModel.cs
@@ -1,7 +1,14 @@
-﻿namespace Snake
+﻿using System.Collections.Generic;
+
+namespace Snake
 {
     public class BodySnakeModel
     {
-        
+        public readonly List<PartOfBodyModel> PartsOfBody;
+
+        public BodySnakeModel()
+        {
+            PartsOfBody = new List<PartOfBodyModel>();
+        }
     }
 }
