d9ad273af738ee16ff5507d60805bbb04c45afcc
Rebrin <rebrin.na@gmail.com>
2023-11-13 01:03:36 +0600

    Created List<PartOfBodySnakeView>

---
+++ Assets/Scripts/Snake/Body/BodySnakeView.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Snake.Body
+{
+    public class BodySnakeView : MonoBehaviour
+    {
+        public PartOfBodySnakeView PartOfBodySnakePrefab;
+        public List<PartOfBodySnakeView> PartsOfBodySnake;
+    }
+}
