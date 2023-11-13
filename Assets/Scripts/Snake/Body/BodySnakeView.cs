2b8b522e3d4e83902f6562b3370ce7b1dfa7aa2f
Rebrin <rebrin.na@gmail.com>
2023-11-13 18:13:32 +0600

    Refactoring

--- Assets/Scripts/Snake/Body/BodySnakeView.cs
+++ Assets/Scripts/Snake/Body/BodySnakeView.cs
@@ -6,6 +6,6 @@ namespace Snake.Body
     public class BodySnakeView : MonoBehaviour
     {
         public PartOfBodySnakeView PartOfBodySnakePrefab;
-        public List<PartOfBodySnakeView> PartsOfBodySnake;
+        public readonly List<PartOfBodySnakeView> PartsOfBodySnake = new();
     }
 }
